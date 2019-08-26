9c7f1f67f5ea7d589ef2a0ca7f2d3c0b88a8e3ce
pensa <fernanpensa@gmail.com>
2019-08-26 15:33:49 -0600

    agregando archivo con funcioon validador primo

--- Inicio.java
+++ Inicio.java
@@ -1,5 +1,8 @@
 public class Inicio{
 	public static void main(String args[]){
 		System.out.println("hola");
+		
+		ValidadorPrimo vp = new ValidadorPrimo();
+		System.out.println(vp.esPrimo(29));
 	}
 }
